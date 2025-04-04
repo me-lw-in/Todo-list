09b767c6ecb01a3ac39580bae50825ce356b756e
Melwin Manish Mendonca <158279734+me-lw-in@users.noreply.github.com>
2025-04-04 21:09:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,16 +25,16 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "public/web-app-manifest-192x192.png",
+            src: "/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            // purpose: "any maskable",
+             purpose: "any maskable",
           },
           {
-            src: "public/web-app-manifest-512x512.png",
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            // purpose: "any maskable",
+             purpose: "any maskable",
           },
         ],
       },
